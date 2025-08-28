58d8a601b55fd18476284adcb630d05463032055
nihal0567 <alexnillc@gmail.com>
2025-08-28 19:18:26 +0600

    added responsive some elements

---
+++ script.js
@@ -0,0 +1,5 @@
+// document.querySelectorAll('.heart').addEventListener('click', function(){
+//     const heartBtn = document.getElementById('heart-btn').innerText;
+//     const newElement = Number(heartBtn) + 1 ;
+//     document.getElementById('heart-btn').innerText = newElement;
+// })
